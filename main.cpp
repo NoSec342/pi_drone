ac7e71b61d93cc7435be8d0e5c8c86bac9cafb3e
NoSec342 <57366092+NoSec342@users.noreply.github.com>
2021-04-27 14:01:51 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -19,34 +19,33 @@ int main(int argc, char **argv)
         std::string buffer;
         buffer << Sock;
         fprintf(stdout , "Received : %s" , buffer.c_str());
-        
+        uint8_t direction = 0;
+        bool is_moving;
         uint8_t rotation = buffer.find("ROTATE_LEFT") != std::string::npos ? buffer.find("ROTATE_RIGHT") != std::string::npos ? 101 : 100 : 0;
-        Motor.write_to_pcb(255);
-        Motor.write_to_pcb(200);
-        Motor.write_to_pcb(rotation);
         if(buffer.find("FRONT") != std::string::npos)
         {
-            
-            Motor.write_to_pcb(0);
-        }
-        if(buffer.find("BACK") != std::string::npos)
+            direction = 0;
+            is_moving = true;
+        }else if(buffer.find("BACK") != std::string::npos)
         {
-         
-            Motor.write_to_pcb(30);
-        }
-        if(buffer.find("LEFT") != std::string::npos)
+            is_moving = true;            
+            direction = 30;
+        }else if(buffer.find("LEFT") != std::string::npos)
         {
-            
-            Motor.write_to_pcb(45);
+            is_moving = true;
+            direction = 45;
         }
         if(buffer.find("RIGHT") != std::string::npos)
         {
-            
-            Motor.write_to_pcb(15);
+            is_moving = true;
+            direction = 15;
         }
-        
         Motor.write_to_pcb(255);
-        
+        Motor.write_to_pcb(100);
+        Motor.write_to_pcb(rotation);
+        Motor.write_to_pcb(direction);        
+        Motor.write_to_pcb(255);
+        is_moving = false;
         if(!Sock.is_client_connected)
         {
             fprintf(stderr, "Client disconnected!\n");
