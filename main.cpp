f9b5358db9faf253d4605ede9fc64e425bb196af
NoSec342 <57366092+NoSec342@users.noreply.github.com>
2021-04-27 09:26:25 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 #include <stdio.h>
 #include "pi_sock.hpp"
+#include "pi_motorctl.hpp"
 #include <thread> 
-
 
 
 int main(int argc, char **argv) 
@@ -11,15 +11,39 @@ int main(int argc, char **argv)
     uint16_t port = argc > 1 ? std::stoi(argv[1]) : 54000;
     pi_sock Sock(port);
     Sock.pi_listen();
-//DUMMY
+    motorctl Motor(10); // TREBUIE SA GASESC UN PIN BUN
+
     while(true)
     {
-        std::cout << Sock;
-        if(!Sock.is_client_connected)
+        
+        std::string buffer;
+        buffer << Sock;
+        uint8_t rotation = buffer.find("ROTATE_LEFT") != std::string::npos ? buffer.find("ROTATE_RIGHT") != std::string::npos ? 101 : 100 : 0;
+        Motor.write_to_pcb(255);
+        Motor.write_to_pcb(200);
+        Motor.write_to_pcb(rotation);
+        if(buffer.find("FRONT") != std::string::npos)
         {
-            fprintf(stderr, "Client Disconnected!\n");
-            return 0;
+            
+            Motor.write_to_pcb(0);
         }
+        if(buffer.find("BACK") != std::string::npos)
+        {
+         
+            Motor.write_to_pcb(30);
+        }
+        if(buffer.find("LEFT") != std::string::npos)
+        {
+            
+            Motor.write_to_pcb(45);
+        }
+        if(buffer.find("RIGHT") != std::string::npos)
+        {
+            
+            Motor.write_to_pcb(15);
+        }
+        
+        Motor.write_to_pcb(255);
     }
     return 0;
 }
